48f26c20e12b862daa00902bceb8085e90cd850a
Wiksa-K <wiki_06@interia.pl>
2025-11-14 19:58:15 +0100

    Add files via upload

---
+++ Lab2_zad7.py
@@ -0,0 +1,6 @@
+Haslo = 'pk47!jy0893'
+
+if len(Haslo) == 11 and '!' in Haslo:
+    print("Hasło jest poprawne")
+else:
+    print("Hasło jest niepoprawne")
---
+++ Lab2_zad8.py
@@ -0,0 +1,7 @@
+text = 'Studiuję-Informatykę'
+
+pierwsze_trzy = text[:3]
+ostatnie_dwa = text[-2:]
+
+print("Pierwsze trzy znaki:", pierwsze_trzy)
+print("Ostatnie dwa znaki:", ostatnie_dwa)
